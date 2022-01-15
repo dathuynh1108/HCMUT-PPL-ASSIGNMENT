0e1a20af4427e99f01dceb916766c62ddd8d6f7e
dat.huynh11082001@hcmut.edu.vn <dat.huynh11082001@hcmut.edu.vn>
2022-01-15 07:23:21 +0700

    Update

--- assignment1/target/D96Visitor.py
+++ assignment1/target/D96Visitor.py
@@ -34,13 +34,13 @@ class D96Visitor(ParseTreeVisitor):
         return self.visitChildren(ctx)
 
 
-    # Visit a parse tree produced by D96Parser#declare_variable_list.
-    def visitDeclare_variable_list(self, ctx:D96Parser.Declare_variable_listContext):
+    # Visit a parse tree produced by D96Parser#attribute_initialization.
+    def visitAttribute_initialization(self, ctx:D96Parser.Attribute_initializationContext):
         return self.visitChildren(ctx)
 
 
-    # Visit a parse tree produced by D96Parser#initialization.
-    def visitInitialization(self, ctx:D96Parser.InitializationContext):
+    # Visit a parse tree produced by D96Parser#attribute_initialization_list.
+    def visitAttribute_initialization_list(self, ctx:D96Parser.Attribute_initialization_listContext):
         return self.visitChildren(ctx)
 
 
@@ -131,6 +131,16 @@ class D96Visitor(ParseTreeVisitor):
 
     # Visit a parse tree produced by D96Parser#variable_and_const_declaration.
     def visitVariable_and_const_declaration(self, ctx:D96Parser.Variable_and_const_declarationContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by D96Parser#variable_initialization.
+    def visitVariable_initialization(self, ctx:D96Parser.Variable_initializationContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by D96Parser#variable_initialization_list.
+    def visitVariable_initialization_list(self, ctx:D96Parser.Variable_initialization_listContext):
         return self.visitChildren(ctx)
 
 
